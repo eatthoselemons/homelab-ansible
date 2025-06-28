066159c776046cea5c1d095557fe65fffba7b2af
Elizabeth Wynn <preevete@gmail.com>
2025-06-27 18:00:56 -0600

    Revert "bootstrap changes"
    This reverts commit dc2a2b3252ad7a7cb7ab55448395b3a9a860c2c0.

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -6,8 +6,8 @@ trap "${trap_msg}" ERR
 virtualEnvPath=$1/ansible-venv
 
 # Install packages needed on a base Debian system
-sudo apt update
-sudo apt --yes install  --no-install-recommends $(
+apt update
+apt --yes install  --no-install-recommends $(
 echo "build-essential
       python3-httpx
       python3-dev
@@ -16,8 +16,10 @@ echo "build-essential
       python3-setuptools
       python3-wheel
       python3-venv
+      sshpass" )
 
 # package requirements
+# python3-httpx - ansible opnsense
 
 mkdir -p ${virtualEnvPath}
 
@@ -28,6 +30,7 @@ ansible-cmdb
 ansible-core
 ansible-lint
 yamllint
+infisical-python
 infisicalsdk
 httpx
 " > ${virtualEnvPath}/requirements.txt
@@ -48,7 +51,6 @@ ${virtualEnvPath}/bin/pip install --upgrade --requirement ${virtualEnvPath}/requ
 
 source ${virtualEnvPath}/bin/activate
 
-mkdir -p $1/git
 if test ! -x $1/git/ansible_systemd; then
   cd $1/git/
   git clone https://github.com/stuvusIT/ansible_systemd.git
@@ -58,5 +60,5 @@ else
 fi
 
 mkdir -p $1/.ansible/roles
-ln -sf  $1/git/ansible_systemd $1/.ansible/roles
+ln -s  $1/git/ansible_systemd $1/.ansible/roles
 
